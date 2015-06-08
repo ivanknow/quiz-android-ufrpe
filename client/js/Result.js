38efdc70c896a63a63647279f1b8ca2a80868ab3
Ivan Rodrigues <ivanknow@gmail.com>
2015-06-08 23:11:01 +0000

    ajustes entrega

--- client/js/Result.js
+++ client/js/Result.js
@@ -2,7 +2,10 @@ ResultController  = {
 		init:function(){
 		    for(var i = 0;i<ProvaExecucaoController.provaSelecionada.questoes.length;i++){
 		        var valor = ProvaExecucaoController.provaSelecionada.questoes[i].resposta||"Sem Resposta";
-		        if(valor === "0"){
+		        var gabarito = ProvaExecucaoController.provaSelecionada.questoes[i].gabarito||"Sem Resposta";
+		        ResultController.addLinhaSuasRespostas(valor);
+		        ResultController.addLinhaGabarito(gabarito);
+		      /*  if(valor === "0"){
 		            $("#resp"+(i+1)).html("A");
 		        }
 		        else if(valor === "1"){
@@ -17,10 +20,46 @@ ResultController  = {
 		            $("#resp"+(i+1)).html("E");
 		        }else{
 		        $("#resp"+(i+1)).html(valor);    
-		        }
+		        }*/
 		        //ProvaExecucaoController.provaSelecionada.questoes[i].resposta||"Sem Resposta");
 		    }
-		}
+		},
+		addLinhaSuasRespostas:function(valor){
+		     if(valor === "0"){
+		            $("#divListaProva").append("<li>A</li>");
+		        }
+		        else if(valor === "1"){
+		           $("#divListaProva").append("<li>B</li>");
+		        }
+		        else if(valor === "2"){
+		            $("#divListaProva").append("<li>C</li>");
+		        }
+		         else if(valor === "3"){
+		             $("#divListaProva").append("<li>D</li>");;
+		        }else if(valor === "4"){
+		            $("#divListaProva").append("<li>E</li>");
+		        }else{
+		       $("#divListaProva").append("<li>"+valor+"</li>"); 
+		        }
+		},
+		addLinhaGabarito:function(valor){
+		     if(valor === "0"){
+		            $("#divListaResposta").append("<li>A</li>");
+		        }
+		        else if(valor === "1"){
+		           $("#divListaResposta").append("<li>B</li>");
+		        }
+		        else if(valor === "2"){
+		            $("#divListaResposta").append("<li>C</li>");
+		        }
+		         else if(valor === "3"){
+		             $("#divListaResposta").append("<li>D</li>");;
+		        }else if(valor === "4"){
+		            $("#divListaResposta").append("<li>E</li>");
+		        }else{
+		       $("#divListaResposta").append("<li>"+valor+"</li>"); 
+		        }
+		},
 };
 
 $(document).on("pagebeforecreate", "#resultado", function() {
